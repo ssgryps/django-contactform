d2151145e587ddd1adcb1f0f0ef2cdfd63ce77cf
ochernia <ochernia@gmail.com>
2019-07-23 16:37:48 +0300

    Change url import

--- contactform/urls.py
+++ contactform/urls.py
@@ -1,5 +1,5 @@
-from django.conf.urls import patterns
+from django.conf.urls import url
 
-urlpatterns = patterns('',
-    (r'^(?P<form_model_id>[0-9]+)/$', 'contactform.views.index')
-)
+urlpatterns = [
+    url(r'^(?P<form_model_id>[0-9]+)/$', 'contactform.views.index')
+]
