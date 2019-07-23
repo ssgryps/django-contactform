24c3f7eae8eebb1c5d8d584fc8286b619c1e4b4b
ochernia <ochernia@gmail.com>
2019-07-23 16:51:48 +0300

    Fi import view

--- contactform/urls.py
+++ contactform/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 
+from contactform.views import index
+
 urlpatterns = [
-    url(r'^(?P<form_model_id>[0-9]+)/$', 'contactform.views.index')
+    url(r'^(?P<form_model_id>[0-9]+)/$', index)
 ]
